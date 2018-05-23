6d1cfc166d3968063417276c4f798ccd6eadc430
Chandana9415 <chandanaravichandra15>
2018-05-22 21:23:39 -0700

    commit and push

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,7 @@ router.get('/', function(req, res, next) {
 var express = require('express');
 var XXX = express.Router();
 var mongodb = require('mongodb');
-var mongoDBURI = process.env.MONGODB_URI || 'mongodb://Chandana:Chandu03!@ds133360.mlab.com:33360/heroku_1zwzjmrz';
+var mongoDBURI = process.env.MONGODB_URI || 'mongodb://Chandana9415:Chandu03!@ds133360.mlab.com:33360/heroku_1zwzjmrz';
 
 
 
