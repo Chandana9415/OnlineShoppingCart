9b6db027cb6ddae02550a8aa7ac4d53221027e33
Chandana9415 <chandanaravichandra15>
2018-05-22 22:41:28 -0700

    commit and push

--- routes/index.js
+++ routes/index.js
@@ -54,7 +54,7 @@ XXX.get('/mongodb', function (request, response) {
 router.post('/storeData', function(req, res, next) {
 //expecting data variable called order--retrieve value using body-parser
 var value_name = req.body.order;  //retrieve the data associated with order
-res.send("order succesfully received: " + value_name);
+res.send("order successfully received: " + value_name);
 });
 
 
