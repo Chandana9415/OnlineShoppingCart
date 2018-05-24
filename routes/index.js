d4d5ae4dc4ceaa03055c1949100d2e5a949771a4
Chandana9415 <chandanaravichandra15>
2018-05-24 15:37:38 -0700

    commit and push

--- routes/index.js
+++ routes/index.js
@@ -2,30 +2,45 @@ var express = require('express');
 var router = express.Router();
 mongodb = require('mongodb');
 
-var mongoDBURI = process.env.MONGODB_URI || 'mongodb://Chandana:Chandu03!@ds133360.mlab.com:33360/heroku_1zwzjmrz?authMechanism=SCRAM-SHA-1';
+var mongoDBURI = process.env.MONGODB_URI || 'mongodb://sharanya16:sAsh5+enkA@ds225840.mlab.com:25840/heroku_mgjkmjm6';
 
 
 var controllerMongoCollection = require('../controllers/database'); //load controller code dealing with database mongodb and Routes collection
 
 
 
-/* GET home page. */
 
+/* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
 });
 
 
+// router.post('/storeData', function(req, res, next) {
+// //expecting data variable called order--retrieve value using body-parser
+//     //retrieve the data associated with order
+//     var value_name = req.body.firstname;
+//     var value_name_2 = req.body.lastname;
+//     // var value_name = req.body.address;
+//     // var value_name_2 = req.body.address2;
+//     // var value_name = req.body.city;
+//     // var value_name_2 = req.body.state;
+//     // var value_name = req.body.zipcode;
+//     // var value_name_2 = req.body.telephone;
+//
+//     //res.send("shipping succesfully received: " + value_name+ " "+value_name_2);
+//
+// });
+router.post('/storeData', controllerMongoCollection.saveOrders);
+
+
+
+
 //CODE to route /getAllRoutes to appropriate  Controller function
-//**************************
-//*** mongodb get all of the Routes in Routes collection w
+//**************************************************************************
+//***** mongodb get all of the Routes in Routes collection w
 //      and Render information iwith an ejs view
 router.get('/getAllOrders', controllerMongoCollection.getAllOrders);
-
-router.post('/storeData', function(req, res, next) {
-//expecting data variable called order--retrieve value using body-parser
-    var value_name = req.body.order  //retrieve the data associated with order
-    res.send("order succesfully received: " + value_name);
-});
+router.get('/getSavedOrder', controllerMongoCollection.getSavedOrder);
 
 module.exports = router;
